302429d7dbaf73565a1bc427e705e8ea6f0c1267
Thanh Le <nhatthanhh1993@gmail.com>
2019-08-11 02:01:00 +0300

    Image Upload

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,4 +1,4 @@
-const { db } = require("../util/admin");
+const { admin, db } = require("../util/admin");
 
 const firebaseConfig = require("../util/firebaseConfig");
 const firebase = require("firebase");
@@ -8,16 +8,16 @@ const { validateSignUpData, validateLogInData } = require("../util/validators");
 
 exports.signUp = (req, res) => {
   const newUser = {
-    email: req.body.email, 
+    email: req.body.email,
     password: req.body.password,
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle
   };
 
   const { valid, errors } = validateSignUpData(newUser);
-  if (!valid) {
-    return res.status(400).json(errors);
-  }
+  if (!valid) return res.status(400).json(errors);
+
+  const noImg = "no-img.png";
 
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
@@ -42,6 +42,9 @@ exports.signUp = (req, res) => {
               handle: newUser.handle,
               email: newUser.email,
               createdAt: new Date().toISOString(),
+              imageUrl: `https://firebasestorage.googleapis.com/v0/b/${
+                firebaseConfig.storageBucket
+              }/o/${noImg}?alt=media`,
               userId
             };
             return db.doc(`/users/${newUser.handle}`).set(userCredentials);
@@ -70,7 +73,7 @@ exports.logIn = (req, res) => {
 
   const { valid, errors } = validateLogInData(user);
   if (!valid) {
-    return res.status(400).json(errors); 
+    return res.status(400).json(errors);
   }
 
   firebase
@@ -90,4 +93,60 @@ exports.logIn = (req, res) => {
           .json({ general: "Email not found, please try again!" });
       else return res.status(500).json({ error: err.code });
     });
+};
+
+exports.uploadImage = (req, res) => {
+  const Busboy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
+
+  let imageFileName;
+  let imageToBeUploaded = {};
+
+  const busboy = new Busboy({ headers: req.headers });
+  busboy.on("file", (fieldName, file, fileName, encoding, mimeType) => {
+    console.log(fieldName);
+    console.log(fileName);
+    console.log(mimeType);
+
+    if (mimeType !== "image/png" && mimeType !== "image/jpeg") {
+      return res.status(400).json({ message: "wrong file type submited" });
+    }
+    imageExtension = fileName.split(".")[fileName.split(".").length - 1];
+    const imageFileName = `${Math.round(
+      Math.random() * 100000000000
+    )}.${imageExtension}`;
+    const filePath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filePath, mimeType };
+    file.pipe(fs.createWriteStream(filePath));
+  });
+
+  busboy.on("finish", () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUploaded.filePath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimeType
+          }
+        }
+      })
+      .then(() => {
+        const imgUrl = `https://firebasestorage.googleapis.com/v0/b/${
+          firebaseConfig.storageBucket
+        }/o/${imageFileName}?alt=media`;
+        return db.doc(`users/${req.user.handle}`).update({ imgUrl });
+      })
+      .then(() => {
+        return res.json({ message: "Image uploaded successfully" });
+      })
+      .catch(err => {
+        console.log(err);
+        return res.status(500).json({ error: err.code });
+      });
+  });
+  busboy.end(req.rawBody);
 };
--- functions/index.js
+++ functions/index.js
@@ -4,22 +4,16 @@ const app = require("express")();
 
 // *** SCREAM ROUTES:
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-
-// get all database of screams and order by createdAt descendingly
-app.get("/screams", getAllScreams);
-
-// post a new scream
 const FBAuth = require("./util/fbAuth"); // Firebase Authentication Midleware to check if user already logined by using idToken
-app.post("/scream", FBAuth, postOneScream);
+
+app.get("/screams", getAllScreams); // get all database of screams and order by createdAt descendingly
+app.post("/scream", FBAuth, postOneScream); // New scream Route
 
 // *** USER ROUTES:
-const { signUp, logIn } = require("./handlers/users");
-
-// SIGNUP Route
-app.post("/signUp", signUp);
-
-//LOGIN Route
-app.post("/logIn", logIn);
+const { signUp, logIn, uploadImage } = require("./handlers/users");
+app.post("/signUp", signUp); // SIGNUP Route
+app.post("/logIn", logIn); //LOGIN Route
+app.post("/user/upload-image", FBAuth, uploadImage);
 
 // export api = route/api/...
 exports.api = functions.https.onRequest(app);
