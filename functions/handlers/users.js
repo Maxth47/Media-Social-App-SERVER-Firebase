02bb0b05c423d60f8c70bcbf89c2d66c69c6d88f
Thanh Le <nhatthanhh1993@gmail.com>
2019-09-02 10:48:45 +0300

    fix upload user image error

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -5,280 +5,276 @@ const firebase = require("firebase");
 firebase.initializeApp(firebaseConfig);
 
 const {
-  validateSignUpData,
-  validateLogInData,
-  reduceUserDetails
+    validateSignUpData,
+    validateLogInData,
+    reduceUserDetails
 } = require("../util/validators");
 
 // Sign user up
 exports.signUp = (req, res) => {
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    handle: req.body.handle
-  };
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    };
 
-  const { valid, errors } = validateSignUpData(newUser);
-  if (!valid) return res.status(400).json(errors);
+    const { valid, errors } = validateSignUpData(newUser);
+    if (!valid) return res.status(400).json(errors);
 
-  const noImg = "no-img.png";
+    const noImg = "no-img.png";
 
-  let token, userId;
-  db.doc(`/users/${newUser.handle}`)
-    .get()
-    .then(doc => {
-      console.log("doc", doc);
-      if (doc.exists) {
-        return res.status(400).json({ handle: "this handle is already taken" });
-      } else {
-        return firebase
-          .auth()
-          .createUserWithEmailAndPassword(newUser.email, newUser.password)
-          .then(data => {
-            userId = data.user.uid;
-            console.log("data.user", data.user);
-            return data.user.getIdToken();
-          })
-          .then(idToken => {
-            console.log("newUser.handle", newUser.handle);
-            token = idToken;
-            const userCredentials = {
-              handle: newUser.handle,
-              email: newUser.email,
-              createdAt: new Date().toISOString(),
-              imageUrl: `https://firebasestorage.googleapis.com/v0/b/${
-                firebaseConfig.storageBucket
-              }/o/${noImg}?alt=media`,
-              userId
-            };
-            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-          })
-          .then(() => {
-            return res.status(201).json({ token });
-          })
-          .catch(err => {
-            if (err.code === "auth/email-already-in-use") {
-              return res
-                .status(400)
-                .json({ email: "this email is already in use" });
+    let token, userId;
+    db.doc(`/users/${newUser.handle}`)
+        .get()
+        .then(doc => {
+            console.log("doc", doc);
+            if (doc.exists) {
+                return res.status(400).json({ handle: "this handle is already taken" });
             } else {
-              return res
-                .status(500)
-                .json({ general: "Something went wrong, please try again" });
+                return firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+                    .then(data => {
+                        userId = data.user.uid;
+                        console.log("data.user", data.user);
+                        return data.user.getIdToken();
+                    })
+                    .then(idToken => {
+                        console.log("newUser.handle", newUser.handle);
+                        token = idToken;
+                        const userCredentials = {
+                            handle: newUser.handle,
+                            email: newUser.email,
+                            createdAt: new Date().toISOString(),
+                            imageUrl: `https://firebasestorage.googleapis.com/v0/b/${
+                                firebaseConfig.storageBucket
+                                }/o/${noImg}?alt=media`,
+                            userId
+                        };
+                        return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+                    })
+                    .then(() => {
+                        return res.status(201).json({ token });
+                    })
+                    .catch(err => {
+                        if (err.code === "auth/email-already-in-use") {
+                            return res
+                                .status(400)
+                                .json({ email: "this email is already in use" });
+                        } else {
+                            return res
+                                .status(500)
+                                .json({ general: "Something went wrong, please try again" });
+                        }
+                    });
             }
-          });
-      }
-    });
+        });
 };
 
 //Log user in
 exports.logIn = (req, res) => {
-  const user = {
-    email: req.body.email,
-    password: req.body.password
-  };
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
 
-  const { valid, errors } = validateLogInData(user);
-  if (!valid) {
-    return res.status(400).json(errors);
-  }
+    const { valid, errors } = validateLogInData(user);
+    if (!valid) {
+        return res.status(400).json(errors);
+    }
 
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(user.email, user.password)
-    .then(data => data.user.getIdToken())
-    .then(token => res.json({ token }))
-    .catch(err => {
-      console.error(err);
-      if (err.code === "auth/wrong-password")
-        return res
-          .status(403)
-          .json({ general: "Wrong password, please try again!" });
-      else if (err.code === "auth/user-not-found")
-        return res
-          .status(403)
-          .json({ general: "Email not found, please try again!" });
-      else return res.status(500).json({ error: err.code });
-    });
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(user.email, user.password)
+        .then(data => data.user.getIdToken())
+        .then(token => res.json({ token }))
+        .catch(err => {
+            console.error(err);
+            if (err.code === "auth/wrong-password" || err.code === "auth/user-not-found" || err.code === "auth/invalid-email")
+                return res
+                    .status(403)
+                    .json({ general: "Wrong creadentials, please try again!" });
+            else return res.status(500).json({ error: err.code });
+        });
 };
 
 // add user details
 exports.addUserDetails = (req, res) => {
-  let userDetails = reduceUserDetails(req.body);
-  db.doc(`/users/${req.user.handle}`)
-    .update(userDetails)
-    .then(() => {
-      return res.json({ message: "Details added successfully" });
-    })
-    .catch(err => {
-      console.error(err);
-      return res.status(500).json({ error: err.code });
-    });
+    let userDetails = reduceUserDetails(req.body);
+    db.doc(`/users/${req.user.handle}`)
+        .update(userDetails)
+        .then(() => {
+            return res.json({ message: "Details added successfully" });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        });
 };
 
 //Get any user's details
 exports.getUserDetails = (req, res) => {
-  let userData = {};
-  db.doc(`/users/${req.params.handle}`)
-    .get()
-    .then(doc => {
-      if (doc.exists) {
-        userData.user = doc.data();
-        return db
-          .collection("screams")
-          .where("userHandle", "==", req.params.handle)
-          .orderBy("createdAt", "desc")
-          .get();
-      } else
-        return res
-          .status(404)
-          .json({ error: `Not found the user - ${req.params.handle}` });
-    })
-    .then(data => {
-      userData.screams = [];
-      data.forEach(doc => {
-        userData.screams.push({
-          body: doc.data().body,
-          createdAt: doc.data().createdAt,
-          userHandle: doc.data().userHandle,
-          userImage: doc.data().userImage,
-          likeCount: doc.data().likeCount,
-          commentCount: doc.data().commentCount,
-          screamId: doc.id
+    let userData = {};
+    db.doc(`/users/${req.params.handle}`)
+        .get()
+        .then(doc => {
+            if (doc.exists) {
+                userData.user = doc.data();
+                return db
+                    .collection("screams")
+                    .where("userHandle", "==", req.params.handle)
+                    .orderBy("createdAt", "desc")
+                    .get();
+            } else
+                return res
+                    .status(404)
+                    .json({ error: `Not found the user - ${req.params.handle}` });
+        })
+        .then(data => {
+            userData.screams = [];
+            data.forEach(doc => {
+                userData.screams.push({
+                    body: doc.data().body,
+                    createdAt: doc.data().createdAt,
+                    userHandle: doc.data().userHandle,
+                    userImage: doc.data().userImage,
+                    likeCount: doc.data().likeCount,
+                    commentCount: doc.data().commentCount,
+                    screamId: doc.id
+                });
+            });
+            return res.json(userData);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
         });
-      });
-      return res.json(userData);
-    })
-    .catch(err => {
-      console.error(err);
-      return res.status(500).json({ error: err.code });
-    });
 };
 
 // Get own user details
 exports.getAuthenticatedUser = (req, res) => {
-  let userData = {};
-  db.doc(`/users/${req.user.handle}`)
-    .get()
-    .then(doc => {
-      if (doc.exists) {
-        userData.credentials = doc.data();
-        return db
-          .collection("likes")
-          .where("userHandle", "==", req.user.handle)
-          .get();
-      }
-    })
-    .then(data => {
-      userData.likes = [];
-      data.forEach(doc => {
-        userData.likes.push(doc.data());
-      });
-      return db
-        .collection("notifications")
-        .where("recipient", "==", req.user.handle)
-        .orderBy("createdAt", "desc")
-        .limit(10)
-        .get();
-    })
-    .then(data => {
-      userData.notifications = [];
-      data.forEach(doc => {
-        userData.notifications.push({
-          recipient: doc.data().recipient,
-          sender: doc.data().sender,
-          read: doc.data().read,
-          screamId: doc.data().screamId,
-          type: doc.data().type,
-          createdAt: doc.data().createdAt,
-          notificationIf: doc.id
+    let userData = {};
+    db.doc(`/users/${req.user.handle}`)
+        .get()
+        .then(doc => {
+            if (doc.exists) {
+                userData.credentials = doc.data();
+                return db
+                    .collection("likes")
+                    .where("userHandle", "==", req.user.handle)
+                    .get();
+            }
+        })
+        .then(data => {
+            userData.likes = [];
+            data.forEach(doc => {
+                userData.likes.push(doc.data());
+            });
+            return db
+                .collection("notifications")
+                .where("recipient", "==", req.user.handle)
+                .orderBy("createdAt", "desc")
+                .limit(10)
+                .get();
+        })
+        .then(data => {
+            userData.notifications = [];
+            data.forEach(doc => {
+                userData.notifications.push({
+                    recipient: doc.data().recipient,
+                    sender: doc.data().sender,
+                    read: doc.data().read,
+                    screamId: doc.data().screamId,
+                    type: doc.data().type,
+                    createdAt: doc.data().createdAt,
+                    notificationIf: doc.id
+                });
+            });
+            return res.json(userData);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
         });
-      });
-      return res.json(userData);
-    })
-    .catch(err => {
-      console.error(err);
-      return res.status(500).json({ error: err.code });
-    });
 };
 
 // upload user image
 exports.uploadImage = (req, res) => {
-  const Busboy = require("busboy");
-  const path = require("path");
-  const os = require("os");
-  const x = require("fs");
+    const Busboy = require("busboy");
+    const path = require("path");
+    const os = require("os");
+    const fs = require("fs");
 
-  console.log("path", path);
+    console.log("path", path);
 
-  let imageFileName;
-  let imageToBeUploaded = {};
+    let imageFileName;
+    let imageToBeUploaded = {};
 
-  const busboy = new Busboy({ headers: req.headers });
-  busboy.on("file", (fieldName, file, fileName, encoding, mimeType) => {
-    // check if the type of file is not PNG or JPEG
-    if (mimeType !== "image/png" && mimeType !== "image/jpeg") {
-      return res.status(400).json({ message: "wrong file type submited" });
-    }
-
-    //get the extension and create a new name
-    imageExtension = fileName.split(".")[fileName.split(".").length - 1];
-    const imageFileName = `${Math.round(
-      Math.random() * 100000000000
-    )}.${imageExtension}`;
-
-    // create a file path to upload
-    const filePath = path.join(os.tmpdir(), imageFileName);
-    imageToBeUploaded = { filePath, mimeType };
-
-    // opens up the writeable stream to `output`(filePath), then pipes the POST data to the file
-    file.pipe(fs.createWriteStream(filePath));
-  });
-
-  busboy.on("finish", () => {
-    // upload the file to Firebase
-    admin
-      .storage()
-      .bucket()
-      .upload(imageToBeUploaded.filePath, {
-        resumable: false,
-        metadata: {
-          metadata: {
-            contentType: imageToBeUploaded.mimeType
-          }
+    const busboy = new Busboy({ headers: req.headers });
+    busboy.on("file", (fieldName, file, fileName, encoding, mimeType) => {
+        // check if the type of file is not PNG or JPEG
+        if (mimeType !== "image/png" && mimeType !== "image/jpeg") {
+            return res.status(400).json({ message: "wrong file type submited" });
         }
-      })
-      .then(() => {
-        const imgUrl = `https://firebasestorage.googleapis.com/v0/b/${
-          firebaseConfig.storageBucket
-        }/o/${imageFileName}?alt=media`;
-        return db.doc(`users/${req.user.handle}`).update({ imgUrl });
-      })
-      .then(() => {
-        return res.json({ message: "Image uploaded successfully" });
-      })
-      .catch(err => {
-        console.log(err);
-        return res.status(500).json({ error: err.code });
-      });
-  });
-  busboy.end(req.rawBody);
+
+        //get the extension and create a new name
+        imageExtension = fileName.split(".")[fileName.split(".").length - 1];
+        const imageFileName = `${Math.round(
+            Math.random() * 100000000000
+        )}.${imageExtension}`;
+
+        // create a file path to upload
+        const filePath = path.join(os.tmpdir(), imageFileName);
+        imageToBeUploaded = { filePath, mimeType };
+
+        // opens up the writeable stream to `output`(filePath), then pipes the POST data to the file
+        file.pipe(fs.createWriteStream(filePath));
+    });
+
+    busboy.on("finish", () => {
+        // upload the file to Firebase
+        admin
+            .storage()
+            .bucket()
+            .upload(imageToBeUploaded.filePath, {
+                resumable: false,
+                metadata: {
+                    metadata: {
+                        contentType: imageToBeUploaded.mimeType
+                    }
+                }
+            })
+            .then(() => {
+                const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${
+                    firebaseConfig.storageBucket
+                    }/o/${imageFileName}?alt=media`;
+                return db.doc(`users/${req.user.handle}`).update({ imageUrl });
+            }) 
+            .then(() => {
+                return res.json({ message: "Image uploaded successfully" });
+            })
+            .catch(err => {
+                console.log(err);
+                return res.status(500).json({ error: err.code });
+            });
+    });
+    busboy.end(req.rawBody);
 };
 
 exports.markNotificationsRead = (req, res) => {
-  let batch = db.batch();
-  req.body.forEach(notificationId => {
-    const notification = db.doc(`/notifications/${notificationId}`);
-    batch.update(notification, { read: true });
-  });
-  batch
-    .commit()
-    .then(() => {
-      return res.json({ message: "Notification marked read" });
-    })
-    .catch(err => {
-      console.error(err);
-      return res.status(500).json({ error: err.code });
+    let batch = db.batch();
+    req.body.forEach(notificationId => {
+        const notification = db.doc(`/notifications/${notificationId}`);
+        batch.update(notification, { read: true });
     });
+    batch
+        .commit()
+        .then(() => {
+            return res.json({ message: "Notification marked read" });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        });
 };
--- functions/index.js
+++ functions/index.js
@@ -6,13 +6,13 @@ const { db } = require("./util/admin");
 
 // *** SCREAM ROUTES:
 const {
-  getAllScreams,
-  postOneScream,
-  getScream,
-  commentOnScream,
-  likeScream,
-  unlikeScream,
-  deleteScream
+    getAllScreams,
+    postOneScream,
+    getScream,
+    commentOnScream,
+    likeScream,
+    unlikeScream,
+    deleteScream
 } = require("./handlers/screams");
 const FBAuth = require("./util/fbAuth"); // Firebase Authentication Midleware to check if user already logined by using idToken
 
@@ -26,18 +26,18 @@ app.post("/scream/:screamId/comment", FBAuth, commentOnScream); // Route to comm
 
 // *** USER ROUTES:
 const {
-  signUp,
-  logIn,
-  uploadImage,
-  addUserDetails,
-  getAuthenticatedUser,
-  getUserDetails,
-  markNotificationsRead
+    signUp,
+    logIn,
+    uploadImage,
+    addUserDetails,
+    getAuthenticatedUser,
+    getUserDetails,
+    markNotificationsRead
 } = require("./handlers/users");
 
 app.post("/signUp", signUp); // SIGNUP Route
 app.post("/logIn", logIn); //LOGIN Route
-app.post("/user/upload-image", FBAuth, uploadImage); // Route to upload image
+app.post("/user/image", FBAuth, uploadImage); // Route to upload image
 app.post("/user", FBAuth, addUserDetails); //Route to add user detail
 app.get("/user", FBAuth, getAuthenticatedUser); //Route to get user detail
 app.get("/user/:handle", getUserDetails); //Route to get user detail
@@ -48,125 +48,125 @@ exports.api = functions.https.onRequest(app);
 
 //like notification
 exports.createNotificationOnLike = functions
-  .region("europe-west1")
-  .firestore.document(`likes/{id}`)
-  .onCreate(snapshot => {
-    return db
-      .doc(`/screams/${snapshot.data().screamId}`)
-      .get()
-      .then(doc => {
-        if (
-          doc.exists &&
-          doc.data().userHandle !== snapshot.data().userHandle
-        ) {
-          return db.doc(`/notifications/${snapshot.id}`).set({
-            createdAt: new Date().toISOString(),
-            recipient: doc.data().userHandle,
-            sender: snapshot.data().userHandle,
-            type: "like",
-            read: false,
-            screamId: doc.id
-          });
-        }
-      })
-      .catch(err => console.error(err));
-  });
+    .region("europe-west1")
+    .firestore.document(`likes/{id}`)
+    .onCreate(snapshot => {
+        return db
+            .doc(`/screams/${snapshot.data().screamId}`)
+            .get()
+            .then(doc => {
+                if (
+                    doc.exists &&
+                    doc.data().userHandle !== snapshot.data().userHandle
+                ) {
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: "like",
+                        read: false,
+                        screamId: doc.id
+                    });
+                }
+            })
+            .catch(err => console.error(err));
+    });
 
 //delete notification once unlike
 exports.deleteNotificationOnUnlike = functions
-  .region("europe-west1")
-  .firestore.document("likes/{id}")
-  .onDelete(snapshot => {
-    return db
-      .doc(`notifications/${snapshot.id}`)
-      .delete()
-      .catch(err => console.error(err));
-  });
+    .region("europe-west1")
+    .firestore.document("likes/{id}")
+    .onDelete(snapshot => {
+        return db
+            .doc(`notifications/${snapshot.id}`)
+            .delete()
+            .catch(err => console.error(err));
+    });
 
 // comment notification
 exports.createNotificationOnComment = functions
-  .region("europe-west1")
-  .firestore.document(`comments/{id}`)
-  .onCreate(snapshot => {
-    return db
-      .doc(`/screams/${snapshot.data().screamId}`)
-      .get()
-      .then(doc => {
-        if (
-          doc.exists &&
-          doc.data().userHandle !== snapshot.data().userHandle
-        ) {
-          return db.doc(`/notifications/${snapshot.id}`).set({
-            createdAt: new Date().toISOString(),
-            recipient: doc.data().userHandle,
-            sender: snapshot.data().userHandle,
-            type: "comment",
-            read: false,
-            screamId: doc.id
-          });
-        }
-      })
-      .catch(err => console.error(err));
-  });
+    .region("europe-west1")
+    .firestore.document(`comments/{id}`)
+    .onCreate(snapshot => {
+        return db
+            .doc(`/screams/${snapshot.data().screamId}`)
+            .get()
+            .then(doc => {
+                if (
+                    doc.exists &&
+                    doc.data().userHandle !== snapshot.data().userHandle
+                ) {
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: "comment",
+                        read: false,
+                        screamId: doc.id
+                    });
+                }
+            })
+            .catch(err => console.error(err));
+    });
 
 //Once user's image changed => update the new user image URL to all related screams
 exports.onUserImageChange = functions
-  .region("europe-west1")
-  .firestore.document(`/users/{userId}`)
-  .onUpdate(change => {
-    console.log(change.before.data());
-    console.log(change.after.data());
-    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
-      console.log("user image changed !!!");
-      const batch = db.batch();
-      return db
-        .collection("screams")
-        .where("userHandle", "==", change.before.data().handle)
-        .get()
-        .then(data => {
-          data.forEach(doc => {
-            const scream = db.doc(`/screams/${doc.id}`);
-            batch.update(scream, { userImage: change.after.data().imageUrl });
-          });
-          return batch.commit();
-        });
-    } else return true;
-  });
+    .region("europe-west1")
+    .firestore.document(`/users/{userId}`)
+    .onUpdate(change => {
+        console.log(change.before.data());
+        console.log(change.after.data());
+        if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+            console.log("user image changed !!!");
+            const batch = db.batch();
+            return db
+                .collection("screams")
+                .where("userHandle", "==", change.before.data().handle)
+                .get()
+                .then(data => {
+                    data.forEach(doc => {
+                        const scream = db.doc(`/screams/${doc.id}`);
+                        batch.update(scream, { userImage: change.after.data().imageUrl });
+                    });
+                    return batch.commit();
+                });
+        } else return true;
+    });
 
 //Once a scream deleted => delete comments, likes, notifications on the scream.
 exports.onScreamDelete = functions
-  .region("europe-west1")
-  .firestore.document(`/screams/{screamId}`)
-  .onDelete((snapshot, context) => {
-    const screamId = context.params.screamId;
-    const batch = db.batch();
-    return db
-      .collection("comments")
-      .where("screamId", "==", screamId)
-      .get()
-      .then(data => {
-        data.forEach(doc => {
-          batch.delete(db.doc(`/comments/${doc.id}`));
-        });
+    .region("europe-west1")
+    .firestore.document(`/screams/{screamId}`)
+    .onDelete((snapshot, context) => {
+        const screamId = context.params.screamId;
+        const batch = db.batch();
         return db
-          .collection("likes")
-          .where("screamId", "==", screamId)
-          .get();
-      })
-      .then(data => {
-        data.forEach(doc => {
-          batch.delete(db.doc(`/likes/${doc.id}`));
-        });
-        return db
-          .collection("notifications")
-          .where("screamId", "==", screamId)
-          .get();
-      })
-      .then(data => {
-        data.forEach(doc => {
-          batch.delete(db.doc(`/notifications/${doc.id}`));
-        });
-        return batch.commit();
-      })
-      .catch(err => console.error(err));
-  });
+            .collection("comments")
+            .where("screamId", "==", screamId)
+            .get()
+            .then(data => {
+                data.forEach(doc => {
+                    batch.delete(db.doc(`/comments/${doc.id}`));
+                });
+                return db
+                    .collection("likes")
+                    .where("screamId", "==", screamId)
+                    .get();
+            })
+            .then(data => {
+                data.forEach(doc => {
+                    batch.delete(db.doc(`/likes/${doc.id}`));
+                });
+                return db
+                    .collection("notifications")
+                    .where("screamId", "==", screamId)
+                    .get();
+            })
+            .then(data => {
+                data.forEach(doc => {
+                    batch.delete(db.doc(`/notifications/${doc.id}`));
+                });
+                return batch.commit();
+            })
+            .catch(err => console.error(err));
+    });
